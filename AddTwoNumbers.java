c3552f351baa7a3ce3b9c1595e0f7d196a365c12
LukeBelieves <lukechuanluo@gmail.com>
2015-04-30 22:10:49 +0800

    new

---
+++ AddTwoNumbers.java
@@ -0,0 +1,35 @@
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){
+		val = x;
+		next = null;
+	}
+}
+
+public class AddTwoNumbers{
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+		ListNode dummy = new ListNode(0);
+		ListNode cur = dummy;
+		int carry = 0;
+		while(l1!=null || l2!=null || carry!=0){
+			int sum = carry;
+			if(l1!=null){
+				sum += l1.val;
+				l1 = l1.next;
+			}
+			if(l2!=null){
+				sum += l2.val;
+				l2 = l2.next;
+			}
+			carry = sum/10;
+			sum = sum%10;
+			ListNode node = new ListNode(sum);
+			cur.next = node;
+			cur = cur.next;
+		}
+		
+		
+		return dummy.next;
+	}
+}

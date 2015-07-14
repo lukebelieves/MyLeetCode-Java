82fa7da8a6fd4b9b71a5314198c2a631cd062957
LukeBelieves <lukechuanluo@gmail.com>
2015-07-14 19:16:00 +0800

    JumpGameII

--- JumpGameII.java
+++ JumpGameII.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class JumpGameII{
 	public int jump(int[] nums){
 		int N = nums.length;
@@ -18,5 +20,25 @@ public class JumpGameII{
 		else{
 			return -1;
 		}
+	}
+	
+	public int jump2(int[] nums){
+		int N = nums.length;
+		if(N==0) return -1;
+		int maxDistance = 0;
+		int lastDistance = 0;
+		int best = 0;
+		ArrayList<Integer> steps = new ArrayList<Integer>();
+		for(int i = 0; i < N && maxDistance >= i; ++i){
+			if(lastDistance < i){
+				steps.add(best);
+				lastDistance = maxDistance;
+			}
+			if(i+nums[i]>maxDistance){
+				maxDistance = i+nums[i];
+				best = i;
+			}
+		}
+		return steps.size();
 	}
 }
